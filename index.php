754f68e97f528c7317e99b85f18082302ca6b957
baku67 <basile08@hotmail.fr>
2023-02-16 11:34:33 +0100

    Formulaire (pas de "e" pour type=number, required, etc)

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>App Web 1</title>
+    </head>
+
+    <body>
+        <form method="post" action="traitement.php" style="display:flex; flex-direction:column; width:70vw; margin:0 auto;">
+            <label for="productName">Nom du produit:</label>
+            <input id="productName" name="productName" type="text" required>
+
+            <label for="unitPrice">Prix à l'unité:</label>
+            <input id="unitPrice" name="unitPrice" type="number" onkeydown="return event.keyCode !== 69" required>
+
+            <label for="quantity">Quantité désirée:</label>
+            <input id="quantity" name="quantity" type="number" onkeydown="return event.keyCode !== 69" required>
+            <br>
+            <input type="submit" >
+        </form>
+    </body>
+</html>
--- traitement.php
+++ traitement.php
@@ -1,1 +1,13 @@
 <?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        if (($_POST["productName"] === null) || ($_POST["unitPrice"] === null) || ($_POST["quantity"] === null)) {
+            echo "Un des champ est vide";
+        }
+
+        $poductName = $_POST["productName"];
+        $unitPrice = $_POST["unitPrice"];
+        $quantity = $_POST["quantity"];
+
+    }
