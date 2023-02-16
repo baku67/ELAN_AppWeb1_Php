cbd1d9fece1d7b9988dba476ac29bce1b76847fe
baku67 <basile08@hotmail.fr>
2023-02-16 16:31:25 +0100

    WIP CSS shoppingCart

--- recap.php
+++ recap.php
@@ -12,6 +12,7 @@
     </head>
 
     <body>
+        <h1>Panier:</h1>
         <?php
             // echo "Affichage variable \$_SESSION['product']<br>";
             // echo print_r($_SESSION["product"]) . "<br><br>";
@@ -23,7 +24,7 @@
             }
             else {
                 echo 
-                "<table>
+                "<div id='tableDiv'><table>
                     <thead>
                         <tr>
                             <th>#</th>
@@ -38,16 +39,16 @@
                     $totalGeneral = 0;
                     foreach ($_SESSION["product"] as $index => $product) {
                         echo 
-                        "<tr>",
-                            "<td>".$index."</td>",
+                        "<tr class='shoppingCartLine'>",
+                            "<td class='index'>".$index."</td>",
                             "<td>".$product["productName"]."</td>",
                             "<td>".number_format($product["unitPrice"], 2, ",", "&nbsp;")."&nbsp;€</td>",
                             "<td>".$product["quantity"]."</td>",
                             "<td>".number_format($product["total"], 2, ",", "&nbsp;")."&nbsp;€</td>",
                             // Suppression d'un produit dans le panier:
-                            "<td><a id='deleteProduct' href='traitement.php?action=deleteProduct&id=" . $index . "'>&times;</a></td>",
-                            "<td><a id='deleteProduct' href='traitement.php?action=minus1&id=" . $index . "'>-</a></td>",
-                            "<td><a id='deleteProduct' href='traitement.php?action=plus1&id=" . $index . "'>+</a></td>",
+                            "<td><a href='traitement.php?action=deleteProduct&id=" . $index . "' class='buttonShoppingCart deleteProduct'>&times;</a></td>",
+                            "<td><a href='traitement.php?action=minus1&id=" . $index . "' class='buttonShoppingCart minus1Product'>-</a></td>",
+                            "<td><a href='traitement.php?action=plus1&id=" . $index . "' class='buttonShoppingCart plus1Product'>+</a></td>",
                         "</tr>";
                         $totalGeneral += $product["total"];
                     }
@@ -57,13 +58,15 @@
                         <td><strong>" .number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€</strong></td>
                     </tr>
                 </tbody>
-                </table>";
+                </table></div>";
 
             }
         ?>
 
-        
+        <br><br>
         <a href="traitement.php?action=clearShoppingCart">Vider le panier</a>
+        <br>
+        <a href="index.php">Retour à l'accueil</a>
 
 
         
