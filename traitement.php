0b3e770e41156bf0e6a1b9fe081a1aa54b827b95
baku67 <basile08@hotmail.fr>
2023-02-16 12:02:05 +0100

    WIP traitement.php

--- traitement.php
+++ traitement.php
@@ -1,13 +1,26 @@
 <?php
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    session_start();
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // if(isset($_POST['submit'])) {
+
+        // En plus du *required* Front
         if (($_POST["productName"] === null) || ($_POST["unitPrice"] === null) || ($_POST["quantity"] === null)) {
             echo "Un des champ est vide";
         }
 
-        $poductName = $_POST["productName"];
-        $unitPrice = $_POST["unitPrice"];
-        $quantity = $_POST["quantity"];
+        // $productName = $_POST["productName"];
+        $productName = filter_input(INPUT_POST, "productName", FILTER_UNSAFE_RAW);
+        // $unitPrice = $_POST["unitPrice"];
+        $unitPrice = filter_input(INPUT_POST, "unitPrice", FILTER_VALIDATE_FLOAT);
+        // $quantity = $_POST["quantity"];
+        $quantity = filter_input(INPUT_POST, "quantity", FILTER_VALIDATE_INT);
 
+        $newProduct = [$productName, $unitPrice, $quantity];
+        $_SESSION["user"]= $newProduct;
+        echo implode($_SESSION["user"]);
+    
     }
+
+    header("Location:index.php");
